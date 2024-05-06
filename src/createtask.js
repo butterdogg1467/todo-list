4cb6792bbf15ab3d9feab6e7ee1d231229db2b5d
butterdogg1467 <momink2600@gmail.com>
2024-05-05 23:07:15 -0400

    Added date input and note input to task

--- src/createtask.js
+++ src/createtask.js
@@ -13,6 +13,15 @@ export default function createTask() {
         let taskTitle = document.createElement('input')
         taskTitle.classList.add('tasktitle')
         taskBackground.appendChild(taskTitle)
+
+        let taskDate = document.createElement('input')
+        taskDate.type = 'date'
+        taskDate.classList.add('taskdate')
+        taskBackground.appendChild(taskDate)
+
+        let taskNote = document.createElement('input')
+        taskNote.classList.add('tasknote')
+        taskBackground.appendChild(taskNote)
     } else if (taskCount >= 63) {
         return
     }
